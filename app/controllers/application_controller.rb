1e47860ed84f2fd42dbbd9e47f30743d0aa51460
odraude7 <odraudelp@gmail.com>
2016-05-22 19:08:31 -0300

    Uma ajeitada na pagina de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -17,6 +18,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
+  end
+
+  def layout_by_resource
+    if devise_controller? && action_name == "new"
+      "signin"
+    else
+      "application"
+    end
   end
 
 end
