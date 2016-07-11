3106a001cf04ef377cc8b9d9f5dad4bfd84f02ae
odraude7 <odraudelp@gmail.com>
2016-07-11 02:53:10 -0300

    Acho que melhorou um pouco a criação do orçamento

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -3,12 +3,27 @@ $(document).ready(function() {
         block_to_be_added.fadeIn('slow');
     });
 
-    $('#blocks').bind('cocoon:after-insert', function(e, added_block) {
-        //added_block.css("background","red");
+    $('.block_links').bind('cocoon:after-insert', function(e, added_block) {
+        added_block.append("<br> <hr> <br>");
+        added_block.prepend("<h4> Novo Bloco </h4>");
     });
 
     $('#blocks').on('cocoon:before-remove', function(e, block) {
         $(this).data('remove-timeout', 1000);
         block.fadeOut('slow');
     })
+
+    $(".add_images a").
+      data("association-insertion-method", 'prepend').
+      data("association-insertion-node", $(".order_images_form") );
+
+    $(".add_modulos a").
+      data("association-insertion-method", 'prepend').
+      data("association-insertion-node", $(".components_form") );
+
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $('#').tooltip('show')
 });
